f842211c5e742911d0fa3f4476c8630b8db123b1
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2022-06-30 19:44:25 +0530

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from 'dotenv';
 import { Keypair, Transaction } from '@solana/web3.js';
 
@@ -7,12 +8,27 @@ const PORT = 4000;
 
 config();
 
+const whitelist = ['http://localhost:3001', 'https://dogeo.dogetrack.io'];
+
 // Allow JSON body
 app.use(express.json());
 
 app.get('/', (_, res) => {
   res.status(203).json({ ping: 'pong' });
 });
+
+// CORS
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (origin && whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 
 app.post('/sign', (req, res) => {
   try {
@@ -30,8 +46,11 @@ app.post('/sign', (req, res) => {
         Uint8Array.from(JSON.parse(process.env.AUTHORITY_KEYPAIR))
       )
     );
-    return res.send({ txData: tx.serialize({ requireAllSignatures: false }) });
+    return res.send({
+      txData: tx.serialize({ requireAllSignatures: false }),
+    });
   } catch (err) {
+    console.log(err);
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 });
